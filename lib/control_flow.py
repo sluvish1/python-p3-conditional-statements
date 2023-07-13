c0ed3ad983cd84b7cb66684bbc5d49ec3f08de94
Vaschell Mkenzie <mvaschell@gmail.com>
2023-07-13 12:29:50 -0400

    finished

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-   if ((username == "admin" or username == "ADMIN")and password == "12345"):
-    return "Access granted"
-   else:
-    return "Access denied"
+    if ((username == "admin" or username == "ADMIN") and password == "12345"):
+        return "Access granted"
+    else:
+        return "Access denied"
+
 
 def hows_the_weather(temperature):
     if temperature < 40:
@@ -16,8 +17,9 @@ def hows_the_weather(temperature):
     else:
         return "It's perfect out there!"
 
+
 def fizzbuzz(num):
-    if (num%3 == 0 and num % 5 == 0):
+    if (num % 3 == 0 and num % 5 == 0):
         return "FizzBuzz"
     elif num % 3 == 0:
         return "Fizz"
@@ -26,12 +28,34 @@ def fizzbuzz(num):
     else:
         return num
 
+
 def calculator(operation, num1, num2):
- dict_map={
-    "+": 
-    "-":
-    "*":
-    "/":
-    else:
-        return""
- }
+    dict_map = {
+        "+": num1 + num2,
+        "-": num1 - num2,
+        "*": num1 * num2,
+        "/": num1/num2
+    }
+    results = dict_map.get(operation,"Invalid operation!") 
+    if results == "Invalid operation!":
+        print("Invalid operation!")
+        return None
+    return results
+        
+    #if the operation is invalid print out Invalid operation and return None
+    #.get goes though the dic_map and looks for the operator the user calls for,
+    #  if that operator isnt listed itll return "invalid operation"
+    #results value would be based upon the opertor if valid itll equal value but if invalid itll equal "invalid operation"
+    #return dict_map[operation]
+
+
+calculator("+", 1, 2)
+# match operation:
+#     case "+":
+#         return num1+num2
+#     case "-":
+#         return num1-num2
+#     case "*":
+#         return num1*num2
+#     case "/":
+#         return num1/num2
